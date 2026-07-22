49788f9532a00039f2480ecb040a1ecfe9fa894c
fernando benegas <benegasfernando1993@gmail.com>
2026-07-22 12:06:45 -0300

    agregamos favicon ultimo cambio por hoy

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: "Frío Total",
   description:
     "Servicio técnico especializado en instalación, reparación y mantenimiento de equipos de refrigeración y aire acondicionado.",
+    icons: {
+    icon: "/frio.ico", // 👈 referencia al archivo en public/
+  }
 };
 
 export default function RootLayout({
